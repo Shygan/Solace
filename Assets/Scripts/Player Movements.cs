90dc9c560fce0c8848892a14f92ea3401a675b70
Shygan <shaunganyewhan@gmail.com>
2025-07-27 18:26:25 +0800

    Update Player Movements.cs
    Added comments to code to remind me of the understanding

--- Assets/Scripts/Player Movements.cs
+++ Assets/Scripts/Player Movements.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float Speed = 5f;
+    [SerializeField] private float Speed = 5f; // [SerializeField] so we can edit in Unity itself
     [SerializeField] private float Jump = 7f;
     private PlayerMovement playerMovement;
     private Rigidbody2D rb;
@@ -13,7 +13,7 @@ public class Player : MonoBehaviour
     {
         playerMovement = new PlayerMovement();
         rb = GetComponent<Rigidbody2D>();
-        originalScale = transform.localScale; //Stores original scale from Unity
+        originalScale = transform.localScale; // Stores original scale from Unity
     }
 
     private void OnEnable()
@@ -31,12 +31,12 @@ public class Player : MonoBehaviour
         Vector2 move = playerMovement.Land.Move.ReadValue<Vector2>();
         float horizontalInput = move.x;
 
-        rb.linearVelocity = new Vector2(horizontalInput * Speed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(horizontalInput * Speed, rb.linearVelocity.y); // Vector2 as rb is a 2D property
 
         if (horizontalInput > 0.01f)
             transform.localScale = originalScale;
         else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z);
+            transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z); // Has to be Vector3 as even tho its 2D, transform.localScale is a 3D property
 
         if (playerMovement.Land.Jump.triggered)
             {
