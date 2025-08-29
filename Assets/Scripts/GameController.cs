79e5c38dbd73ca2e20c768a130eadb822190ab3e
Shygan <shaunganyewhan@gmail.com>
2025-08-29 18:23:09 +0800

    Completed the first level (Level 1)
    Did Skeleton for Level 2, but still have to make the options

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,12 +6,18 @@ public class GameController : MonoBehaviour
 {
     int progressAmount;
     public Slider progressSlider;
+    public GameObject player;
+    public GameObject LoadCanvas;
+    public List<GameObject> levels;
+    private int currentLevelIndex = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         progressAmount = 0;
         progressSlider.value = 0;
         Apple.OnAppleCollect += IncreaseProgressAmount;
+        HoldToLoadLevel.OnHoldComplete += LoadNextLevel;
+        LoadCanvas.SetActive(false);
         Debug.Log("Start");
     }
 
@@ -29,7 +36,23 @@ public class GameController : MonoBehaviour
         if (progressAmount >= 100)
         {
             // Level is complete
+            LoadCanvas.SetActive(true);
             Debug.Log("Level Complete");
         }
+    }
+
+    void LoadNextLevel()
+    {
+        int nextLevelIndex = (currentLevelIndex == levels.Count - 1) ? 0 : currentLevelIndex + 1;
+        LoadCanvas.SetActive(false);
+
+        levels[currentLevelIndex].SetActive(false);
+        levels[nextLevelIndex].SetActive(true);
+
+        player.transform.position = new Vector3(0, 0, 0);
+
+        currentLevelIndex = nextLevelIndex;
+        progressAmount = 0;
+        progressSlider.value = 0;
     }
 }
